fc2e4c1f0ce3b6d465f6d35aa765c213c803cf8f
shkeeb122 <shkeebshah326@gmail.com>
2026-03-26 10:50:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ MISTRAL_API_KEY = os.environ.get("MISTRAL_API_KEY")
 MISTRAL_URL = "https://api.mistral.ai/v1/chat/completions"
 MODEL_NAME = "mistral-small-latest"
 
+BACKEND_URL = os.environ.get("BACKEND_URL", "https://umar-k20u.onrender.com")
+
 HEADERS = {
     "Authorization": f"Bearer {MISTRAL_API_KEY}",
     "Content-Type": "application/json"
 }
-
-BACKEND_URL = os.environ.get("BACKEND_URL", "https://umar-k20u.onrender.com")
